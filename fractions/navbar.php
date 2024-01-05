ceb2dbaa2d002ba2a3c6b063abd523361e706854
Paramak <lakeza551@gmail.com>
2024-01-05 15:41:46 +0700

    add responsive navbar

--- fractions/navbar.php
+++ fractions/navbar.php
@@ -1,5 +1,5 @@
 <?php include 'topbar.php'?>
-<header class="navbar">
+<nav class="navbar">
     <div class="navbar-image-container">
         <img src="./assets/images/uamc-removebg-preview.png" alt="">
     </div>
@@ -59,9 +59,57 @@
             </div>
         </a>
     </div>
+    <button class="navbar-hamburger" onClick="toggleHamburgerList()">
+        <div></div>
+        <div></div>
+        <div></div>
+    </button>
+    <div class="navbar-hamburger-list" style="display: none">
+        <a href="./">
+            <h3>Home</h3>
+        </a>
+        <a href="./importantDates.php">
+            <h3>Important Dates</h3>
+        </a>
+        <a href="./conferrence.php">
+            <h3>Conference Session</h3>
+        </a>
+        <a href="./schedule.php">
+            <h3>Schedule</h3>
+        </a>
+        <a href="./registration.php">
+            <h3>Registration</h3>
+        </a>
+        <a href="./servey.php">
+            <h3>Servey</h3>
+        </a>
+        <a href="./download.php"> 
+            <h3>Download</h3>
+        </a>
+        <a href="./venue.php"> 
+            <h3>Venue</h3>
+        </a>
+        <a href="./#contact">
+            <h3>Contact Us</h3>
+        </a>
+    </div>
     <script>
-        const option = document.querySelector(`[href='.${window.location.pathname}']`)
+        const navbarOptions = document.querySelectorAll(`[href='.${window.location.pathname}']`)
+        const option = navbarOptions[0]
         option.querySelector('span').style.color = '#fd7e14'
         option.querySelector('.navbar-option-slideIn').style.width = '100%'
+
+        const hamburgerOption = navbarOptions[1]
+        hamburgerOption.style.backgroundColor = 'white'
+        hamburgerOption.querySelector('h3').style.color = '#fd7e14'
+
+        function toggleHamburgerList() {
+            const listElement = document.querySelector('.navbar-hamburger-list')
+            const listState = listElement.style.display
+            if(listState === 'none')
+                listElement.style.display = 'block'
+            else
+                listElement.style.display = 'none'
+        }
     </script>
-</header>
+</nav>
